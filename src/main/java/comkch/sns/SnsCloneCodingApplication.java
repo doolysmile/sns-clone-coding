cfec3debdb59b5e8220bef06de58f13c72678a57
KCH <doolysmile1@naver.com>
2022-07-03 23:38:57 +0900

    Initial commit

---
+++ src/main/java/comkch/sns/SnsCloneCodingApplication.java
@@ -0,0 +1,13 @@
+package comkch.sns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnsCloneCodingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnsCloneCodingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/comkch/sns/SnsCloneCodingApplicationTests.java
@@ -0,0 +1,13 @@
+package comkch.sns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnsCloneCodingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
